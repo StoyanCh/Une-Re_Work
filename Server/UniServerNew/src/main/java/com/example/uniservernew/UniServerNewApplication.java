ed948f60796434719b8a49a5e353879cc6e3ee6a
Stoyan Chompov <stoqnchompov@gmail.com>
2025-06-19 23:29:30 +0300

    Server Created, connected to database, next step Entities

---
+++ Server/UniServerNew/src/main/java/com/example/uniservernew/UniServerNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniservernew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniServerNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniServerNewApplication.class, args);
+    }
+
+}
---
+++ Server/UniServerNew/src/test/java/com/example/uniservernew/UniServerNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniservernew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniServerNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
